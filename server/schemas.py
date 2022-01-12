a109b371c00b75a7af6214789e87c09fd318227c
Stamper <stamper@ukr.net>
2022-01-12 14:17:41 +0200

    iron solid storage

--- server/schemas.py
+++ server/schemas.py
@@ -5,3 +5,25 @@ class Package(BaseModel):
     command: str
     id: str = ""
     message: str = ""
+
+
+class Storage(dict):
+    def __init__(self):
+        super().__init__()
+        self._keys = []
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self._keys.append(key)
+
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        self._keys.remove(key)
+
+    def pop(self, key):
+        self._keys.remove(key)
+        return super().pop(key)
+
+    def values(self):
+        for key in self._keys:
+            yield self.__getitem__(key)
